79e44eadfa89b7c109d66158026840d14c5a6bc6
rafael-sperafico <153711081+rafael-sperafico@users.noreply.github.com>
2024-01-25 10:03:37 +0200

    Add tests

---
+++ calculator.test.mjs
@@ -0,0 +1,13 @@
+import test from "ava";
+
+const macro = test.macro((t, input, expected) => {
+	t.is(eval(input), expected);
+});
+
+test('sum', macro, '1 + 2', 3);
+
+test('subtract', macro, '3 - 2', 1);
+
+test('multiply', macro, '3 * 5', 15);
+
+test('divide', macro, '8 / 2', 4);

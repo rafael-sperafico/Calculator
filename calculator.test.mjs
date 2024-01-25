201a113164dcbc7f9ea0a1d51905df6b02f49b6e
rafael-sperafico <rafael.sperafico@ambientia.fi>
2024-01-25 13:55:02 +0200

    CALC-3 #comment That implementation was difficult #in-progress

--- calculator.test.mjs
+++ calculator.test.mjs
@@ -6,7 +6,7 @@ const macro = test.macro((t, input, expected) => {
 
 test('sum', macro, '1 + 2', 3);
 
-// #test('subtract', macro, '3 - 2', 1);
+test('subtract', macro, '3 - 2', 1);
 
 // test('multiply', macro, '3 * 5', 15);
 

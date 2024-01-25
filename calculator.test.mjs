4c15be78cfc75688de78a49d98491eb25f47601f
rafael-sperafico <153711081+rafael-sperafico@users.noreply.github.com>
2024-01-25 10:06:39 +0200

    Update calculator.test.mjs

--- calculator.test.mjs
+++ calculator.test.mjs
@@ -4,10 +4,10 @@ const macro = test.macro((t, input, expected) => {
 	t.is(eval(input), expected);
 });
 
-test('sum', macro, '1 + 2', 3);
+// test('sum', macro, '1 + 2', 3);
 
-test('subtract', macro, '3 - 2', 1);
+// #test('subtract', macro, '3 - 2', 1);
 
-test('multiply', macro, '3 * 5', 15);
+// test('multiply', macro, '3 * 5', 15);
 
-test('divide', macro, '8 / 2', 4);
+// test('divide', macro, '8 / 2', 4);
